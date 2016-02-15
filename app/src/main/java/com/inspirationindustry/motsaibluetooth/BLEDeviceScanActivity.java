9601da92d7b410f051bd1093d800451ffb157330
Scott Laughlin <scott.laughlin15@gmail.com>
2016-02-15 17:55:40 -0500

    Updates: - After discovery, user can click on device name in a list - App connects to GATT server - Callback detects connection state changes - Callback detects discovered services

--- app/src/main/java/com/inspirationindustry/motsaibluetooth/BLEDeviceScanActivity.java
+++ app/src/main/java/com/inspirationindustry/motsaibluetooth/BLEDeviceScanActivity.java
@@ -3,7 +3,11 @@ package com.inspirationindustry.motsaibluetooth;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,10 +16,12 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import butterknife.OnClick;
 
@@ -28,6 +34,31 @@ public class BLEDeviceScanActivity extends ListActivity {
     private ArrayAdapter<String> mLeDeviceListAdapter;
     private static final long SCAN_PERIOD = 60000;
     private List<BluetoothDevice> mDeviceList;
+    private BluetoothGatt mBluetoothGatt;
+
+    //GATT CALLBACK VARIABLES
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+    private final static String TAG = BluetoothLeService.class.getSimpleName();
+    private BluetoothManager mBluetoothManager;
+    private String mBluetoothDeviceAddress;
+    private int mConnectionState = STATE_DISCONNECTED;
+
+    //0x2A19 is the battery life characteristic
+    //Another option based on using AT+CHAR? gives -> 0xFFE1 for the HM-10 characteristic
+    public final static UUID UUID_BLE_CHARACTERISTIC = UUID.nameUUIDFromBytes(hexStringToByteArray("0x2A19"));
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,14 +128,113 @@ public class BLEDeviceScanActivity extends ListActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Log.w("BLUETOOTH DEBUG", "YOU FOUND SOMETHING!");
+                            Log.w("BLUETOOTH DEBUG", "You found something! Running LeScan Callback");
+
                             mLeDeviceListAdapter.add(device.getName().toString());
-                            mDeviceList.add(device);
                             mLeDeviceListAdapter.notifyDataSetChanged();
+
+                            mDeviceList.add(device);
+
+
+
                         }
                     });
                 }
             };
+
+    //GATT CALLBACK
+    private final BluetoothGattCallback mGattCallback =
+            new BluetoothGattCallback() {
+                @Override
+                public void onConnectionStateChange(BluetoothGatt gatt, int status,
+                                                    int newState) {
+                    String intentAction;
+                    Log.w("BLUETOOTH DEBUG", "You are in onConnectionStateChange");
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        intentAction = ACTION_GATT_CONNECTED;
+                        mConnectionState = STATE_CONNECTED;
+                        broadcastUpdate(intentAction);
+                        Log.i(TAG, "Connected to GATT server.");
+                        Log.i(TAG, "Attempting to start service discovery: " +
+                                mBluetoothGatt.discoverServices());
+
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        intentAction = ACTION_GATT_DISCONNECTED;
+                        mConnectionState = STATE_DISCONNECTED;
+                        Log.i(TAG, "Disconnected from GATT server.");
+                        broadcastUpdate(intentAction);
+                    }
+                }
+
+                @Override
+                // New services discovered
+                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                    Log.w("BLUETOOTH DEBUG", "You are in onServicesDiscovered");
+                    Log.w("BLUETOOTH DEBUG", gatt.getServices().toString());
+                    //Here is what this returned:
+//                    [android.bluetooth.BluetoothGattService@41f52190, android.bluetooth.BluetoothGattService@41f52770, android.bluetooth.BluetoothGattService@41f52d08]
+//                    [android.bluetooth.BluetoothGattService@41f52190, android.bluetooth.BluetoothGattService@41f52770, android.bluetooth.BluetoothGattService@41f52d08]
+//                    [android.bluetooth.BluetoothGattService@41f52190, android.bluetooth.BluetoothGattService@41f52770, android.bluetooth.BluetoothGattService@41f52d08]
+
+                    //TODO
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+                    } else {
+                        Log.w(TAG, "onServicesDiscovered received: " + status);
+                    }
+                }
+
+                @Override
+                // Result of a characteristic read operation
+                public void onCharacteristicRead(BluetoothGatt gatt,
+                                                 BluetoothGattCharacteristic characteristic,
+                                                 int status) {
+                    Log.w("BLUETOOTH DEBUG", "You are in onCharacteristicRead");
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+                    }
+                }
+            };
+
+    private void broadcastUpdate(final String action) {
+        final Intent intent = new Intent(action);
+        Log.w("BLUETOOTH DEBUG", "You are in short form of broadcastUpdate");
+        sendBroadcast(intent);
+    }
+
+    private void broadcastUpdate(final String action,
+                                 final BluetoothGattCharacteristic characteristic) {
+        final Intent intent = new Intent(action);
+        Log.w("BLUETOOTH DEBUG", "You are in onBroadcastUpdate");
+        // This is special handling for the Heart Rate Measurement profile. Data
+        // parsing is carried out as per profile specifications.
+        if (UUID_BLE_CHARACTERISTIC.equals(characteristic.getUuid())) {
+            int flag = characteristic.getProperties();
+            int format = -1;
+            if ((flag & 0x01) != 0) {
+                format = BluetoothGattCharacteristic.FORMAT_UINT16;
+                Log.d(TAG, "Heart rate format UINT16.");
+            } else {
+                format = BluetoothGattCharacteristic.FORMAT_UINT8;
+                Log.d(TAG, "Heart rate format UINT8.");
+            }
+            final int heartRate = characteristic.getIntValue(format, 1);
+            Log.d(TAG, String.format("Received heart rate: %d", heartRate));
+            intent.putExtra(EXTRA_DATA, String.valueOf(heartRate));
+        } else {
+            // For all other profiles, writes the data formatted in HEX.
+            final byte[] data = characteristic.getValue();
+            if (data != null && data.length > 0) {
+                final StringBuilder stringBuilder = new StringBuilder(data.length);
+                for(byte byteChar : data)
+                    stringBuilder.append(String.format("%02X ", byteChar));
+                intent.putExtra(EXTRA_DATA, new String(data) + "\n" +
+                        stringBuilder.toString());
+            }
+        }
+        sendBroadcast(intent);
+    }
+
 
     @OnClick(R.id.refreshButton)
     public void refreshActivity(View view){
@@ -115,8 +245,31 @@ public class BLEDeviceScanActivity extends ListActivity {
     }
 
 
-}
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l,v,position,id);
+
+        BluetoothDevice device = mDeviceList.get(position);
+        //Note: Our app is the GATT client
+        mBluetoothGatt = device.connectGatt(getBaseContext(), false, mGattCallback);
+
+        //Create Toast Message
+        String clicked_device = device.getName();
+        String message = "You clicked on " + clicked_device + " at position " + position;
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
+
+}
 
 
 
